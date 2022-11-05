e75660d37657cbb5fdfc9cd42786c9866f1a0087
Ananya Smirti <43774146+ananyasmirti@users.noreply.github.com>
2022-11-04 21:58:34 -0600

    Update perceptron.py

--- Perceptron/perceptron.py
+++ Perceptron/perceptron.py
@@ -1,128 +1,114 @@
 import pandas as pd
 import numpy as np
 
-def standard(train, test, T,lr):
-    wei = np.zeros(len(train.columns))
-    test_err = dict.fromkeys(i for i in range(1, T))
-    test['1'] = 1
-    col = test.pop('1')
-    test.insert(0, '1', col)
-    teX = np.array(test.drop(['label'], axis=1))
-    
-    for e in range(1, T + 1):
-        shuffle = train.sample(frac=1).reset_index(drop=True)
-        shuffle['1'] = 1
-        col = shuffle.pop('1')
-        shuffle.insert(0, '1', col)
-        X = np.array(shuffle.drop('label', axis=1))
-        y = np.array(shuffle['label'])
-  
-        for i in range(len(shuffle)):
-           
-            error = y[i] * (wei.T.dot(X[i]))
-            if error <= 0:
-              
-                wei = wei + y[i] * X[i]*lr
+def standard(x, y,T,lr):
+    ns= x.shape[0]
+    dim = x.shape[1]
+    wei = np.zeros(dim)
+    b = np.arange(ns)
+    for t in range(T):
+        np.random.shuffle(b)
+        x = x[b,:]
+        y = y[b]
+        for i in range(ns):
+            t = np.sum(x[i] * wei)
+            if not (t * y[i] > 0):
+                wei = wei + lr * y[i] * x[i]
+    return wei
 
-        pred = []
-    
-        for i in range(0, len(teX)):
-            pred.append(np.sign(wei.T.dot(teX[i])))
-        test['pred'] = pred
-
-        test_err[e] = len(test[test['pred'] != test['label']]) / len(test)
-
-    err_a = sum(test_err.values()) / T
-    return wei, err_a
-
-
-def voted(train, test, T,lr):
-    err_test = 0
-    test['1'] = 1
-    col = test.pop('1')
-    test.insert(0, '1', col)
-    teX = np.array(test.drop(['label'], axis=1))
-    wei = np.zeros(len(train.columns))
-    W1 = [wei]
-    m1 = 0
-    C = dict.fromkeys(i for i in range(0, T))
-    
-    for e in range(1, T + 1):
+def voted(x, y,T, lr):
+    ns = x.shape[0]
+    m = x.shape[1]
+    wei = np.zeros(m)
+    b = np.arange(ns)
+    list_c = np.array([])
+    list_w = np.array([])
+    c = 0
+    for t in range(T):
         
-        shuffle= train.reset_index(drop=True)
-        shuffle['1'] = 1
-        col = shuffle.pop('1')
-        shuffle.insert(0, '1', col)
-        X = np.array(shuffle.drop('label', axis=1))
-        y = np.array(shuffle['label'])
-
-        for i in range(len(X)):
-            error = y[i] * (W1[m1].T.dot(X[i]))
-            if error <= 0:
-                W1.append(W1[m1] + y[i] * X[i]*lr)
-                m1 = m1 + 1
-                C[m1] = 1
+        x = x[b,:]
+        y = y[b]
+        for i in range(ns):
+            tmp = np.sum(x[i] * wei)
+            if not (tmp * y[i] > 0):
+                list_w = np.append(list_w, wei)
+                list_c = np.append(list_c, c)
+                wei = wei + lr * y[i] * x[i]
+                c = 1
             else:
-                C[m1] = C[m1] + 1
-        pred_f = []
-        for j in range(len(teX)):
-            pred = []
-            for i in range(1, len(W1)):
-                pred.append(C[i] * np.sign(W1[i].T.dot(teX[j])))
-            pred_f.append(np.sign(sum(pred)))
-        test['pred'] = pred_f
-        err_test += len(test[test['pred'] != test['label']]) / len(test)
-
-    return W1, C, (err_test / T)
-
-
-def averaged(train, test, T,lr):
+                c = c + 1
+    n1 = list_c.shape[0]
+    list_w = np.reshape(list_w, (n1,-1))
+    return list_c, list_w
     
-    err_test = 0
-    test['1'] = 1
-    col = test.pop('1')
-    test.insert(0, '1', col)
-    teX = np.array(test.drop(['label'], axis=1))
-    wei = np.zeros(len(train.columns))
-    wef = np.zeros(len(train.columns))
-    for e in range(1, T + 1):
-        shuffle = train.reset_index(drop=True)
-        shuffle['1'] = 1
-        col = shuffle.pop('1')
-        shuffle.insert(0, '1', col)
-        X = np.array(shuffle.drop('label', axis=1))
-        y = np.array(shuffle['label'])
-        for i in range(len(X)):
-            err = y[i] * (wei.T.dot(X[i]))
-            if err <= 0:
-                wei = wei + y[i] * X[i]*lr
+def average(x, y,T,lr):
+    ns= x.shape[0]
+    m = x.shape[1]
+    wei = np.zeros(m)
+    a = np.zeros(m)
+    b = np.arange(ns)
+    for t in range(T):
+        
+        x = x[b,:]
+        y = y[b]
+        for i in range(ns):
+            t = np.sum(x[i] * wei)
+            if not (t * y[i] > 0):
+                wei = wei + lr * y[i] * x[i]
+            a = a + wei
+    return a
 
-            wef = wef + wei
+train = pd.read_csv('./bank-note/train.csv', header=None)
+r = train.values
+nc = r.shape[1]
+nr = r.shape[0]
+train_x = np.copy(r)
+train_x[:,nc - 1] = 1
+train_y = r[:, nc - 1]
+train_y = 2 * train_y - 1
 
-        pred_f = []
-        for j in range(len(teX)):
-            pred_f.append(np.sign(wef.T.dot(teX[j])))
-        test['pred'] = pred_f
-        err_test += len(test[test['pred'] != test['label']]) / len(test)
-    return wef, (err_test/ T)
+test = pd.read_csv('./bank-note/test.csv', header=None)
+r = test.values
+nc = r.shape[1]
+num_row = r.shape[0]
+test_x = np.copy(r)
+test_x[:,nc - 1] = 1
+test_y = r[:, nc - 1]
+test_y = 2 * test_y - 1
 
+print("=================Standard Perceptron==================")
+wei = standard(train_x, train_y,10,0.01)
+wei = np.reshape(wei, (-1,1))
+pred = np.matmul(test_x, wei)
+pred[pred > 0] = 1
+pred[pred <= 0] = -1
+error = np.sum(np.abs(pred - np.reshape(test_y,(-1,1)))) / 2 / test_y.shape[0]
+print('Test Error:', error)
+print('Weights:',wei)
 
-train = pd.read_csv('./bank-note/train.csv', names=['variance', 'skewness', 'curtosis', 'entropy', 'label'])
-test = pd.read_csv('./bank-note/test.csv', names=['variance', 'skewness', 'curtosis', 'entropy', 'label'])
+print("=================Voted Perceptron==================")
+c_list, w_list = voted(train_x, train_y,10,0.01)
+c_list = np.reshape(c_list, (-1,1))
+print('Weights:')
+print(w_list)
+w_list = np.transpose(w_list)
+pred1 = np.matmul(test_x, w_list)
+pred1[pred1 >0] = 1
+pred1[pred1 <=0] = -1
+voted = np.matmul(pred1, c_list)
+voted[voted >0] = 1
+voted[voted<=0] = -1
+error = np.sum(np.abs(voted - np.reshape(test_y,(-1,1)))) / 2 / test_y.shape[0]
+print('Count')
+print(c_list)
+print('Test Error',error)
 
-train.loc[train.label == 0, "label"] = -1
-test.loc[test.label == 0, "label"] = -1
-
-print("================Standard Perceptron=================")
-w,e=standard(train.copy(), test.copy(), 10,0.01)
-print("Learned Weight Vector", w)
-print("Test Error", e)
-print("================Voted Perceptron=================")
-w,c,e=voted(train.copy(), test.copy(), 10,0.01)
-print("Learned Weight Vector", w)
-print("Counts", c)
-print("Test Error", e)
-print("================Averaged Perceptron=================")
-w,e=averaged(train.copy(), test.copy(), 10,0.01)
-print("Learned Weight Vector", w)
-print("Test Error", e)
+print("=================Average Perceptron==================")
+wei1 = average(train_x, train_y,10,0.01)
+wei1 = np.reshape(wei1, (-1,1))
+pred = np.matmul(test_x, wei1)
+pred[pred > 0] = 1
+pred[pred <= 0] = -1
+err = np.sum(np.abs(pred - np.reshape(test_y,(-1,1)))) / 2 / test_y.shape[0]
+print('Test Error:', error)
+print('Weights:',wei1)
